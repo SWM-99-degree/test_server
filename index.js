bba9ebf6b369a25807fd3ff56c7c8a7db8f08580
lineno2 <hosun1804@gmail.com>
2023-08-04 11:02:26 +0900

    [FIX] header set error fixed

--- index.js
+++ index.js
@@ -23,57 +23,51 @@ const callbackUrlScheme = 'jaribean';
 
 //enable cors for all origins
 app.get('/', (req, res) => {
-        res.send(`<a href="https://kauth.kakao.com/oauth/authorize?client_id=${process.env.KAKAO_REST_API_KEY}&redirect_uri=${process.env.KAKAO_REDIRECT_URI}&response_type=code">Login with Kakao</a>`);
-    }
+    res.send(`<a href="https://kauth.kakao.com/oauth/authorize?client_id=${process.env.KAKAO_REST_API_KEY}&redirect_uri=${process.env.KAKAO_REDIRECT_URI}&response_type=code">Login with Kakao</a>`);
+}
 );
 
 app.get('/oauth/kakao', async (req, res) => {
-    try{
+    try {
         //get code from queryparams
         const { code } = req.query;
         res.redirect(`${callbackUrlScheme}://code?code=${code}&type=kakao`);
     }
-    catch(error){
+    catch (error) {
         console.log(error);
     }
 });
 
-app.post('/user/login', async (req, res) => {
-    try{
+app.post('/user/login/kakao', async (req, res) => {
+    try {
         const { type, code } = req.body;
-        switch(type){
-            case 'kakao':
-                const{
-                    data: {access_token : kakaoAccessToken}
-                } = await axios('https://kauth.kakao.com/oauth/token', {
-                    params: {
-                        grant_type: 'authorization_code',
-                        client_id: process.env.KAKAO_REST_API_KEY,
-                        redirect_uri: process.env.KAKAO_REDIRECT_URI,
-                        code: code
-                    },
-                });
-        
-                const { data : kakaoUser } = await axios('https://kapi.kakao.com/v2/user/me', {
-                    headers: {
-                        Authorization: `Bearer ${kakaoAccessToken}`,
-                    },
-                });
-                userId = kakaoUser.id;
-                userName = kakaoUser.properties.nickname;
-                break;
-            default:
-                res.status(502).send('login should have type');
-                return;
-        }
-        const accessToken = jwt.sign({userId, userName}, process.env.JWT_SECRET);
-        const refreshToken = jwt.sign({userId, userName}, process.env.JWT_SECRET);
+        const {
+            data: { access_token: kakaoAccessToken }
+        } = await axios('https://kauth.kakao.com/oauth/token', {
+            params: {
+                grant_type: 'authorization_code',
+                client_id: process.env.KAKAO_REST_API_KEY,
+                redirect_uri: process.env.KAKAO_REDIRECT_URI,
+                code: code
+            },
+        });
+
+        const { data: kakaoUser } = await axios('https://kapi.kakao.com/v2/user/me', {
+            headers: {
+                Authorization: `Bearer ${kakaoAccessToken}`,
+            },
+        });
+        userId = kakaoUser.id;
+        userName = kakaoUser.properties.nickname;
+
+        const accessToken = jwt.sign({ userId, userName }, process.env.JWT_SECRET);
+        const refreshToken = jwt.sign({ userId, userName }, process.env.JWT_SECRET);
         res.header('Content-Type', 'application/json');
         res.send(JSON.stringify({ accessToken, refreshToken }));
     }
-    catch(error){
+    catch (error) {
         res.status(502).send('error');
-    }  
+    }
 });
 
 //open port 3000 for listening
